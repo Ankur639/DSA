b5a0431a22072dbf56fea3f4db61104e8769629a
Ankur639 <kumar.ankur727@gmail.com>
2025-10-26 19:35:10 +0530

    First commit

---
+++ Aditya_Verma_Series/BINARY_SEARCH/search_in_descending_array.js
@@ -0,0 +1,20 @@
+let arr = [20, 17, 15, 14, 13, 12, 10, 9, 8, 4, 2];
+
+let target = 2;
+
+function binarySearch(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((high + low) / 2);
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] > target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch(arr, target));
